218d68a75675083527b2b0f5abfd7eff88a70b62
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-14 06:48:28 +0700

    update handleCreateLobby handler to save host and lobby

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -6,11 +6,13 @@ import {
     WebSocketServer,
     OnGatewayConnection,
     OnGatewayDisconnect,
+    ConnectedSocket,
 } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
-import { Logger, UseGuards } from "@nestjs/common";
+import { Inject, Logger, UseGuards } from "@nestjs/common";
 import { JwtWsGuard } from "../auth/guard/jwt-ws.guard";
 import { type JwtUser, User } from "../auth/user.decorator";
+import Redis from "ioredis";
 
 @WebSocketGateway()
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -19,28 +21,37 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     private logger = new Logger("GameGateway");
 
-    constructor(private lobbyService: LobbyService) {}
+    constructor(
+        private lobbyService: LobbyService,
+        @Inject("REDIS_CLIENT") private redis: Redis,
+    ) {}
 
     handleConnection(client: Socket) {
         this.logger.log(`Client connected: ${client.id}`);
     }
 
-    async handleDisconnect(client: Socket) {
-        
-    }
+    async handleDisconnect(client: Socket) {}
 
     @UseGuards(JwtWsGuard)
     @SubscribeMessage("createLobby")
     async handleCreateLobby(
         @User() user: JwtUser,
         @MessageBody() payload: { quizId: number },
+        @ConnectedSocket() client: Socket,
     ) {
         const { quizId } = payload;
         const hostId = user.id;
 
         const lobby = await this.lobbyService.createLobby({ quizId, hostId });
+
+        // [TO-DO]: Save host and lobbyId to redis,
+        // the app needs to keep track of the host socket id with the lobby id
+        // so that it can destroy the lobby when the host disconnects
+        await this.redis.set(
+            `${client.id}`,
+            JSON.stringify({ isHost: true, lobbyId: lobby.id }),
+        );
+
         return { lobby };
     }
-
-
 }
