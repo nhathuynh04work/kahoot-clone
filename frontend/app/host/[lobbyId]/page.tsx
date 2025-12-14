c933f55c8998f337d852e32ae3332117f0d63db6
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-14 16:15:41 +0700

    implement basic logic for the HostGameScreen

--- frontend/app/host/[lobbyId]/page.tsx
+++ frontend/app/host/[lobbyId]/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { socket } from "@/features/game/lib/socket";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function HostGameScreen() {
-    
+	const { lobbyId } = useParams();
+	const router = useRouter();
+
+	if (!lobbyId) {
+		router.push("/dashboard");
+	}
+
+	useEffect(() => {
+		socket.emit("hostJoin", { lobbyId }, (response: any) => {
+			if (!response.success) {
+				router.push("/dashboard");
+			}
+		});
+	}, [lobbyId, router]);
 }
