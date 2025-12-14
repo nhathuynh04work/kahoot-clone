b78d49e76016d5d2759a9aa684a71c24d8094f88
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-14 08:17:53 +0700

    start all over with game feature

--- frontend/features/game/api/server-actions.ts
+++ frontend/features/game/api/server-actions.ts
@@ -6,6 +6,6 @@ export async function getValidLobby(
 	pin: string
 ): Promise<GameLobby & { quiz: Quiz }> {
 	const api = await apiServer();
-	const { data } = await api.get(`/game/validate-pin?pin=${pin}`);
+	const { data } = await api.get(`/game/lobby?pin=${pin}`);
 	return data;
 }
