16da977f32a7c9cc179061ed36a4a8c26c964bc9
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-16 15:46:25 +0700

    implement question screen for player

--- frontend/app/game/page.tsx
+++ frontend/app/game/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+import { PlayerQuestionScreen } from "@/features/game/components/player/player-question-screen";
 import { PlayerWaitingScreen } from "@/features/game/components/player/player-waiting-screen";
 import { usePlayerGame } from "@/features/game/hooks/use-player-game";
 
 export default function PlayerGameScreen() {
-	const { state } = usePlayerGame();
+	const { state, handlers } = usePlayerGame();
 
 	switch (state.status) {
 		case "WAITING":
 			return <PlayerWaitingScreen nickname={state.nickname} />;
+
+		case "QUESTION":
+			return (
+				<PlayerQuestionScreen
+					question={state.currentQuestion!}
+					onSubmit={handlers.handleSelectOption}
+				/>
+			);
 	}
 }
